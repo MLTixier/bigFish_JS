45852e32233d0ccd8be1409ad04ee174b0b67995
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-04-05 16:23:18 +0200

    feat: #7 carousel improved with API's images - WIP

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 $(document).ready(function () {
     ajax();
-    $('#refresh').click(function(){
+    $('#refresh').click(function () {
         ajax();
     });
 
-   function ajax(){
-       $.ajax({
-           url: "https://www.fishwatch.gov/api/species",
-           method: "GET",
-           dataType: "json",
-       })
-           .done(function (response) {
-               afficheTout(response);
-           })
-           .fail(function (error) {
-               alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-           })
-   }
+    function ajax() {
+        $.ajax({
+            url: "https://www.fishwatch.gov/api/species",
+            method: "GET",
+            dataType: "json",
+        })
+            .done(function (response) {
+                afficheTout(response);
+                afficheCarousel(response);
+            })
+            .fail(function (error) {
+                alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+            })
+    }
+
+    //affichage des images issues de l'API dans le carousel :
+    function afficheCarousel(jsonDatas) {
+        for (let i in jsonDatas) {
+            if (jsonDatas[i]['Image Gallery'] != null) {
+                const image = document.createElement("img");
+                if (jsonDatas[i]['Image Gallery'].length > 1) {
+                    const fishImage = document.createElement("img");
+                    fishImage.src = jsonDatas[i]['Image Gallery'][0].src;
+                    fishImage.alt = jsonDatas[i]['Image Gallery'][0].alt;
+                } else if (jsonDatas[i]['Image Gallery'].length == 0) {
+                    const fishImage = document.createElement("img");
+                    fishImage.src = jsonDatas[i]['Image Gallery'].src;
+                    fishImage.alt = jsonDatas[i]['Image Gallery'].alt;
+                }
+                image.appendChild(fishImage);
+                $("#carousel").append(affichePoisson(i, jsonDatas));
+            }
+        }
+    }
 
     //affichage d'un article :
     function affichePoisson(i, jsonDatas) {
-        const fishCard = document.createElement("div")
+        const fishCard = document.createElement("div");
 
         const fishTitle = document.createElement("h3");
         fishTitle.innerHTML = jsonDatas[i]['Species Name'];
