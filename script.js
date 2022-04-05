efb1da3122e5a65d755b8318ec2463d1d265d7a4
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-04-05 16:03:53 +0200

    feat: #4 feed improved with API's elements

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
-$_request
+$(document).ready(function () {
+    ajax();
+    $('#refresh').click(function(){
+        ajax();
+    });
 
-$(document).ready(function(){
-
-    let jsonDatas="";
-
-    let apiRequest = $.ajax({
-        url: "https://www.fishwatch.gov/api/species.json",
-        method: "GET",
-        dataType: "json",
-    })
-
-    apiRequest.fail(function (error) {
-        alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    })
-
-    apiRequest.done(function (response) {
-        // jsonDatas = JSON.stringify(response);
-        jsonDatas = response;
-        //$("div.feeder").append(jsonDatas);
-    })
-
-    console.log(jsonDatas);
+   function ajax(){
+       $.ajax({
+           url: "https://www.fishwatch.gov/api/species",
+           method: "GET",
+           dataType: "json",
+       })
+           .done(function (response) {
+               afficheTout(response);
+           })
+           .fail(function (error) {
+               alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+           })
+   }
 
     //affichage d'un article :
-    function hydratePoisson(i) {
-        const fishCard = document.createElement("div");
-        fishCard.className = "fish-card";
+    function affichePoisson(i, jsonDatas) {
+        const fishCard = document.createElement("div")
 
         const fishTitle = document.createElement("h3");
-        fishTitle.className = "fish-title";
+        fishTitle.innerHTML = jsonDatas[i]['Species Name'];
+        fishCard.append(fishTitle);
+
+        if (jsonDatas[i]['Image Gallery'] != null) {
+            if (jsonDatas[i]['Image Gallery'].length > 1) {
+                const fishImage = document.createElement("img");
+                fishImage.src = jsonDatas[i]['Image Gallery'][0].src;
+                fishImage.alt = jsonDatas[i]['Image Gallery'][0].alt;
+                fishCard.appendChild(fishImage);
+            } else if (jsonDatas[i]['Image Gallery'].length == 0) {
+                const fishImage = document.createElement("img");
+                fishImage.src = jsonDatas[i]['Image Gallery'].src;
+                fishImage.alt = jsonDatas[i]['Image Gallery'].alt;
+                fishCard.appendChild(fishImage);
+            }
+        } else {
+            const fishImage = document.createElement("img");
+            fishImage.src = "https://www.aquaportail.com/pictures1910/phones/poisson.jpg"
+            fishImage.alt = "photo générique de poisson";
+            fishCard.appendChild(fishImage);
+        }
+
+        const labelList = document.createElement("span");
         const fishLabel = document.createElement("label");
-        fishLabel.className = "fish-label";
+        fishLabel.innerHTML = jsonDatas[i]['Harvest Type'];
+        labelList.append(fishLabel);
+        fishCard.append(labelList);
+
         const fishDescription = document.createElement("p");
-        fishDescription.className = "fish-description";
-        //const fishImage = document.createElement("img");
-        //fishImage.className="fish-image";
+        fishDescription.innerHTML = "Description physique :" + jsonDatas[i]['Physical Description'];
+        fishCard.append(fishDescription);
 
-        fishTitle.innerHTML = jsonDatas[i].SpeciesName;
-        fishLabel.innerHTML = jsonDatas[i].HarvestType;
-        fishDescription.innerHTML = "Description physique :" + jsonDatas[i].PhysicalDescription;
-        //fishImage.src = jsonDatas[i].ImageGallery.src;
-        //fishImage.alt = jsonDatas[i].ImageGallery.alt;
-
-        //j'attribue comme enfants de la sous-div les const définies plus haut
-        fishCard.appendChild(fishTitle);
-        fishCard.appendChild(fishLabel);
-        fishCard.appendChild(fishDescription);
-        //fishCard.appendChild(fishImage);
         return fishCard;
     }
 
@@ -54,10 +62,10 @@ $(document).ready(function(){
     //affichage de tous les articles :
     function afficheTout(jsonDatas) {
         for (let i in jsonDatas) {
-            document.getElementById("#feeder").appendChild(hydratePoisson(i));
+            $("#ajax").append(affichePoisson(i, jsonDatas));
         }
     }
-
-    $('#refresh').click(afficheTout(jsonDatas));
-
 })
+
+
+
