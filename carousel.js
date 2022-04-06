0d2dc6d9c5e03949c8d4129abe81ade2483bc031
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-04-06 16:25:38 +0200

    ammend

--- carousel.js
+++ carousel.js
@@ -1,6 +1,6 @@
 const galleryContainer = document.querySelector('.gallery-container');
 const galleryControlsContainer = document.querySelector('.gallery-controls');
-const galleryControls = ['previous', 'next'];
+const galleryControls = ['precedent', 'suivant'];
 const galleryItems = document.querySelectorAll('.gallery-item');
 
 class Carousel {
