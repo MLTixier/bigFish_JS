c59ecbde0efecbe161a38eeea6a08cadc9363c4c
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-04-07 10:45:15 +0200

    vanilla and jQuery files separated, one unique functions file created

---
+++ scriptVanilla.js
@@ -0,0 +1,12 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+
+        // affiche les 2 premières cartes poissons de l'API au chargement de la page :'
+        apiVanilla(1);
+    }
+};
+
+
+
+
+
